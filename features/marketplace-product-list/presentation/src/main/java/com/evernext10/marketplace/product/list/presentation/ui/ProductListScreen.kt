61be887f4b7e64c08f417f55aeec459dbc6788f4
Evert Moreno <evernext10@gmail.com>
2022-11-25 10:22:53 -0500

    Restore Search View

--- features/marketplace-product-list/presentation/src/main/java/com/evernext10/marketplace/product/list/presentation/ui/ProductListScreen.kt
+++ features/marketplace-product-list/presentation/src/main/java/com/evernext10/marketplace/product/list/presentation/ui/ProductListScreen.kt
@@ -5,11 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.evernext10.core.domain.model.pokemon.Pokemon
 import com.evernext10.core.domain.model.pokemon.state.StatePokemonList
 import com.evernext10.core.ext.launchAndRepeatWithViewLifecycle
+import com.evernext10.core.ext.onQueryTextChanged
 import com.evernext10.core.ext.showAlertDialogErrorApi
 import com.evernext10.core.ext.visible
 import com.evernext10.marketplace.product.list.presentation.R
@@ -68,10 +70,25 @@ class ProductListScreen : Fragment() {
             adapter = adapterProductList
             layoutManager = GridLayoutManager(requireContext(), 2)
         }
+
+        searchProduct.apply {
+            queryHint = HtmlCompat.fromHtml(
+                "<font color = #000000>" + resources.getString(R.string.search_bar_title) + "</font>",
+                HtmlCompat.FROM_HTML_MODE_LEGACY
+            )
+            query
+            onQueryTextChanged {
+                progressBottom.visible(true)
+                productListViewModel.postQueryWord(it)
+            }
+        }
     }
 
     private fun observerState() = with(binding) {
         launchAndRepeatWithViewLifecycle {
+            productListViewModel.queryWord.observe(viewLifecycleOwner) {
+                // productListViewModel.getMarketplaceProductList(it)
+            }
             productListViewModel.productListState.observe(viewLifecycleOwner) {
                 Log.i("ResponseStatus", it.toString())
                 when (it) {
